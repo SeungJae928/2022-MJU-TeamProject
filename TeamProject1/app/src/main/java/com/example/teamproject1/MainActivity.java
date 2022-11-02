9425e003bf6e6991ff2b83d7e8431a0a0b22280c
Bang <owen1743@naver.com>
2022-11-03 00:10:24 +0900

    로그인 페이지 구현

---
+++ TeamProject1/app/src/main/java/com/example/teamproject1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.teamproject1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page_layout);
+    }
+}
