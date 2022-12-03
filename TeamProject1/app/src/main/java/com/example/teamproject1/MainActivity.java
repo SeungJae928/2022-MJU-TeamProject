b0868ce0ded215420d64292361e9dbcaf570a844
SeungJae <owen1743@naver.com>
2022-12-04 00:29:17 +0900

    버튼 추가 함수 구현

--- TeamProject1/app/src/main/java/com/example/teamproject1/MainActivity.java
+++ TeamProject1/app/src/main/java/com/example/teamproject1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.teamproject1;
 
+import static com.example.teamproject1.InfoActivity.station_name;
 import static com.example.teamproject1.LoginActivity.exit_state;
 
 import androidx.appcompat.app.ActionBar;
@@ -12,6 +13,9 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton menuButton1;
@@ -19,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageButton menuButton3;
     private ImageButton menuButton4;
     private ImageButton menuButton5;
+    private List<MyButton> btnlist;
     private Button searchButton;
 
     private final long finishTime= 1000;
@@ -34,6 +39,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.main_page);
         ActionBar actionBar = getSupportActionBar();
 //        actionBar.hide();
+
+        btnlist = this.getButtonList();
+
+        for(MyButton btn : btnlist){
+            btn.getBtn().setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(getApplicationContext(), InfoActivity.class);
+                    startActivity(intent);
+                    station_name = btn.getStation();
+                }
+            });
+        }
 
         menuButton1 = findViewById(R.id.menuButton1);
         menuButton2 = findViewById(R.id.menuButton2);
@@ -124,5 +142,130 @@ public class MainActivity extends AppCompatActivity {
         } else {
             btn_state = true;
         }
+    }
+
+    public List<MyButton> getButtonList() {
+        List<MyButton> list = new ArrayList<>();
+        list.add(new MyButton(findViewById(R.id.station_101), "101"));
+        list.add(new MyButton(findViewById(R.id.station_102), "102"));
+        list.add(new MyButton(findViewById(R.id.station_103), "103"));
+        list.add(new MyButton(findViewById(R.id.station_104), "104"));
+        list.add(new MyButton(findViewById(R.id.station_105), "105"));
+        list.add(new MyButton(findViewById(R.id.station_106), "106"));
+        list.add(new MyButton(findViewById(R.id.station_107), "107"));
+        list.add(new MyButton(findViewById(R.id.station_108), "108"));
+        list.add(new MyButton(findViewById(R.id.station_109), "109"));
+        list.add(new MyButton(findViewById(R.id.station_110), "110"));
+        list.add(new MyButton(findViewById(R.id.station_111), "111"));
+        list.add(new MyButton(findViewById(R.id.station_112), "112"));
+        list.add(new MyButton(findViewById(R.id.station_113), "113"));
+        list.add(new MyButton(findViewById(R.id.station_114), "114"));
+        list.add(new MyButton(findViewById(R.id.station_115), "115"));
+        list.add(new MyButton(findViewById(R.id.station_116), "116"));
+        list.add(new MyButton(findViewById(R.id.station_117), "117"));
+        list.add(new MyButton(findViewById(R.id.station_118), "118"));
+        list.add(new MyButton(findViewById(R.id.station_119), "119"));
+        list.add(new MyButton(findViewById(R.id.station_120), "120"));
+        list.add(new MyButton(findViewById(R.id.station_121), "121"));
+        list.add(new MyButton(findViewById(R.id.station_122), "122"));
+        list.add(new MyButton(findViewById(R.id.station_123), "123"));
+
+        list.add(new MyButton(findViewById(R.id.station_201), "201"));
+        list.add(new MyButton(findViewById(R.id.station_202), "202"));
+        list.add(new MyButton(findViewById(R.id.station_203), "203"));
+        list.add(new MyButton(findViewById(R.id.station_204), "204"));
+        list.add(new MyButton(findViewById(R.id.station_205), "205"));
+        list.add(new MyButton(findViewById(R.id.station_206), "206"));
+        list.add(new MyButton(findViewById(R.id.station_207), "207"));
+        list.add(new MyButton(findViewById(R.id.station_208), "208"));
+        list.add(new MyButton(findViewById(R.id.station_209), "209"));
+        list.add(new MyButton(findViewById(R.id.station_210), "210"));
+        list.add(new MyButton(findViewById(R.id.station_211), "211"));
+        list.add(new MyButton(findViewById(R.id.station_212), "212"));
+        list.add(new MyButton(findViewById(R.id.station_213), "213"));
+        list.add(new MyButton(findViewById(R.id.station_214), "214"));
+        list.add(new MyButton(findViewById(R.id.station_215), "215"));
+        list.add(new MyButton(findViewById(R.id.station_216), "216"));
+        list.add(new MyButton(findViewById(R.id.station_217), "217"));
+
+        list.add(new MyButton(findViewById(R.id.station_301), "301"));
+        list.add(new MyButton(findViewById(R.id.station_302), "302"));
+        list.add(new MyButton(findViewById(R.id.station_303), "303"));
+        list.add(new MyButton(findViewById(R.id.station_304), "304"));
+        list.add(new MyButton(findViewById(R.id.station_305), "305"));
+        list.add(new MyButton(findViewById(R.id.station_306), "306"));
+        list.add(new MyButton(findViewById(R.id.station_307), "307"));
+        list.add(new MyButton(findViewById(R.id.station_308), "308"));
+
+        list.add(new MyButton(findViewById(R.id.station_401), "401"));
+        list.add(new MyButton(findViewById(R.id.station_402), "402"));
+        list.add(new MyButton(findViewById(R.id.station_403), "403"));
+        list.add(new MyButton(findViewById(R.id.station_404), "404"));
+        list.add(new MyButton(findViewById(R.id.station_405), "405"));
+        list.add(new MyButton(findViewById(R.id.station_406), "406"));
+        list.add(new MyButton(findViewById(R.id.station_407), "407"));
+        list.add(new MyButton(findViewById(R.id.station_408), "408"));
+        list.add(new MyButton(findViewById(R.id.station_409), "409"));
+        list.add(new MyButton(findViewById(R.id.station_410), "410"));
+        list.add(new MyButton(findViewById(R.id.station_411), "411"));
+        list.add(new MyButton(findViewById(R.id.station_412), "412"));
+        list.add(new MyButton(findViewById(R.id.station_413), "413"));
+        list.add(new MyButton(findViewById(R.id.station_414), "414"));
+        list.add(new MyButton(findViewById(R.id.station_415), "415"));
+        list.add(new MyButton(findViewById(R.id.station_416), "416"));
+        list.add(new MyButton(findViewById(R.id.station_417), "417"));
+
+        list.add(new MyButton(findViewById(R.id.station_501), "501"));
+        list.add(new MyButton(findViewById(R.id.station_502), "502"));
+        list.add(new MyButton(findViewById(R.id.station_503), "503"));
+        list.add(new MyButton(findViewById(R.id.station_504), "504"));
+        list.add(new MyButton(findViewById(R.id.station_505), "505"));
+        list.add(new MyButton(findViewById(R.id.station_506), "506"));
+        list.add(new MyButton(findViewById(R.id.station_507), "507"));
+
+        list.add(new MyButton(findViewById(R.id.station_601), "601"));
+        list.add(new MyButton(findViewById(R.id.station_602), "602"));
+        list.add(new MyButton(findViewById(R.id.station_603), "603"));
+        list.add(new MyButton(findViewById(R.id.station_604), "604"));
+        list.add(new MyButton(findViewById(R.id.station_605), "605"));
+        list.add(new MyButton(findViewById(R.id.station_606), "606"));
+        list.add(new MyButton(findViewById(R.id.station_607), "607"));
+        list.add(new MyButton(findViewById(R.id.station_608), "608"));
+        list.add(new MyButton(findViewById(R.id.station_609), "609"));
+        list.add(new MyButton(findViewById(R.id.station_610), "610"));
+        list.add(new MyButton(findViewById(R.id.station_611), "611"));
+        list.add(new MyButton(findViewById(R.id.station_612), "612"));
+        list.add(new MyButton(findViewById(R.id.station_613), "613"));
+        list.add(new MyButton(findViewById(R.id.station_614), "614"));
+        list.add(new MyButton(findViewById(R.id.station_615), "615"));
+        list.add(new MyButton(findViewById(R.id.station_616), "616"));
+        list.add(new MyButton(findViewById(R.id.station_617), "617"));
+        list.add(new MyButton(findViewById(R.id.station_618), "618"));
+        list.add(new MyButton(findViewById(R.id.station_619), "619"));
+        list.add(new MyButton(findViewById(R.id.station_620), "620"));
+        list.add(new MyButton(findViewById(R.id.station_621), "621"));
+        list.add(new MyButton(findViewById(R.id.station_622), "622"));
+
+        list.add(new MyButton(findViewById(R.id.station_701), "701"));
+        list.add(new MyButton(findViewById(R.id.station_702), "702"));
+        list.add(new MyButton(findViewById(R.id.station_703), "703"));
+        list.add(new MyButton(findViewById(R.id.station_704), "704"));
+        list.add(new MyButton(findViewById(R.id.station_705), "705"));
+        list.add(new MyButton(findViewById(R.id.station_706), "706"));
+        list.add(new MyButton(findViewById(R.id.station_707), "707"));
+
+        list.add(new MyButton(findViewById(R.id.station_801), "801"));
+        list.add(new MyButton(findViewById(R.id.station_802), "802"));
+        list.add(new MyButton(findViewById(R.id.station_803), "803"));
+        list.add(new MyButton(findViewById(R.id.station_804), "804"));
+        list.add(new MyButton(findViewById(R.id.station_805), "805"));
+        list.add(new MyButton(findViewById(R.id.station_806), "806"));
+
+        list.add(new MyButton(findViewById(R.id.station_901), "901"));
+        list.add(new MyButton(findViewById(R.id.station_902), "902"));
+        list.add(new MyButton(findViewById(R.id.station_903), "903"));
+        list.add(new MyButton(findViewById(R.id.station_904), "904"));
+
+        return list;
     }
 }
---
+++ TeamProject1/app/src/main/java/com/example/teamproject1/MyButton.java
@@ -0,0 +1,30 @@
+package com.example.teamproject1;
+
+import android.widget.Button;
+
+public class MyButton {
+
+    private Button btn;
+    private String station;
+
+    public MyButton(Button btn, String station){
+        this.btn = btn;
+        this.station = station;
+    }
+
+    public Button getBtn() {
+        return btn;
+    }
+
+    public void setBtn(Button btn) {
+        this.btn = btn;
+    }
+
+    public String getStation() {
+        return station;
+    }
+
+    public void setStation(String station) {
+        this.station = station;
+    }
+}
